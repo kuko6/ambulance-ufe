3f3ee86f71c873237091ce550f4350ccf0148208
Jakub Povinec <jakub.povinec@gmail.com>
2024-02-26 12:31:00 +0100

    fixed tests

--- src/components/japo-ambulance-wl-app/test/japo-ambulance-wl-app.spec.tsx
+++ src/components/japo-ambulance-wl-app/test/japo-ambulance-wl-app.spec.tsx
@@ -2,17 +2,27 @@ import { newSpecPage } from '@stencil/core/testing';
 import { JapoAmbulanceWlApp } from '../japo-ambulance-wl-app';
 
 describe('japo-ambulance-wl-app', () => {
-  it('renders', async () => {
+
+  it('renders editor', async () => {
     const page = await newSpecPage({
+      url: `http://localhost/entry/@new`,
       components: [JapoAmbulanceWlApp],
-      html: `<japo-ambulance-wl-app></japo-ambulance-wl-app>`,
+      html: `<japo-ambulance-wl-app base-path="/"></japo-ambulance-wl-app>`,
     });
-    expect(page.root).toEqualHtml(`
-      <japo-ambulance-wl-app>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </japo-ambulance-wl-app>
-    `);
+    page.win.navigation = new EventTarget()
+    const child = await page.root.shadowRoot.firstElementChild;
+    expect(child.tagName.toLocaleLowerCase()).toEqual ("japo-ambulance-wl-editor");
+
+  });
+
+  it('renders list', async () => {
+    const page = await newSpecPage({
+      url: `http://localhost/ambulance-wl/`,
+      components: [JapoAmbulanceWlApp],
+      html: `<japo-ambulance-wl-app base-path="/ambulance-wl/"></japo-ambulance-wl-app>`,
+    });
+    page.win.navigation = new EventTarget()
+    const child = await page.root.shadowRoot.firstElementChild;
+    expect(child.tagName.toLocaleLowerCase()).toEqual("japo-ambulance-wl-list");
   });
 });
--- src/components/japo-ambulance-wl-editor/test/japo-ambulance-wl-editor.spec.tsx
+++ src/components/japo-ambulance-wl-editor/test/japo-ambulance-wl-editor.spec.tsx
@@ -2,17 +2,17 @@ import { newSpecPage } from '@stencil/core/testing';
 import { JapoAmbulanceWlEditor } from '../japo-ambulance-wl-editor';
 
 describe('japo-ambulance-wl-editor', () => {
-  it('renders', async () => {
+  it('buttons shall be of different type', async () => {
     const page = await newSpecPage({
       components: [JapoAmbulanceWlEditor],
-      html: `<japo-ambulance-wl-editor></japo-ambulance-wl-editor>`,
+      html: `<japo-ambulance-wl-editor entry-id="@new"></japo-ambulance-wl-editor>`,
     });
-    expect(page.root).toEqualHtml(`
-      <japo-ambulance-wl-editor>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </japo-ambulance-wl-editor>
-    `);
+    let items: any = await page.root.shadowRoot.querySelectorAll("md-filled-button");
+    expect(items.length).toEqual(1);
+    items = await page.root.shadowRoot.querySelectorAll("md-outlined-button");
+    expect(items.length).toEqual(1);
+
+    items = await page.root.shadowRoot.querySelectorAll("md-filled-tonal-button");
+    expect(items.length).toEqual(1);
   });
 });
